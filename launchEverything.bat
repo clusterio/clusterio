aa93af6d066a72b213bbd5fd056dfd8d0676cc6b
Danielv123 <tranen.bananen@gmail.com>
2016-09-29 09:16:01 +0200

    More cleanup

--- launchEverything.bat
+++ launchEverything.bat
@@ -2,14 +2,4 @@ cd "Factorio_0.13.17"
 @RD /S /Q "temp"
 cd "bin/x64/"
 start factorio.exe --start-server testmap.zip --rcon-port 12345 --rcon-password 123 --server-settings server-settings.json
-cd ..
-cd ..
-cd ..
-
-::start cmd /C node client.js
-::start cmd /C node master.js
-
-::cd "Factorio 0.13.9 - Copy/bin/x64"
-::@start factorio.exe
-
 pause
