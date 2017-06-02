18a40ca80644007f69166dd1a92687c43aab2bed
Danielv123 <tranen.bananen@gmail.com>
2017-06-02 22:29:28 +0200

    More lua experimenting

--- sharedPlugins/inventoryImports/lua commands.lua
+++ sharedPlugins/inventoryImports/lua commands.lua
@@ -5,10 +5,33 @@
 
 -- print JSON of entire inventory and request slots to file
 -- TODO: Get requester slot data as well
+
+-- requester slot data is game.players.character.get_request_slot(slot)
+-- returns {name="copper-plate", count=47} or nil if nothing is set
+-- maybe game.players.character.item_requests? http://lua-api.factorio.com/latest/LuaEntity.html#LuaEntity.item_requests
+
+/c
+local requests = {}
+for i = 1, game.player.force.character_logistic_slot_count do
+	requests[i] = game.players["Danielv123"].character.get_request_slot(i)
+end
+
+local o = "Requests: "
+for k,v in pairs(requests) do
+	o = o..v["name"]..": "..v["count"]..", "
+end
+game.print(o)
+
 /c
 local inventory = game.players["Danielv123"].get_inventory(defines.inventory.player_main).get_contents()
 local quickbar = game.players["Danielv123"].get_quickbar().get_contents()
-local requestSlots = {}
+--[[ Get contents of requester slots into a table ]]
+local requests = {}
+for i = 1, game.player.force.character_logistic_slot_count do
+	requests[i] = game.players["Danielv123"].character.get_request_slot(i)
+end
+
+--[[ generate JS table (not JSON) ]]
 local o = "{inventory:{"
 for k,v in pairs(inventory) do
 	o = o.."['"..k.."']:"..v..","
@@ -16,10 +39,12 @@ end
 for k,v in pairs(quickbar) do
 	o = o.."['"..k.."']:"..v..","
 end
+
 o = o.."},requestSlots:{"
-for k,v in pairs(requestSlots) do
-o = o.."['"..k.."']:"..v..","
+for k,v in pairs(requests) do
+	o = o.."['"..v["name"].."']:"..v["count"]..", "
 end
 o = o.."}}"
+game.print(o)
 game.write_file("test.txt", o)
 
