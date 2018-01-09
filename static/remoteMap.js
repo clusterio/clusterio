9ce7cf5f287de30ac79d72701e8c977bc603b84d
Danielv123 <tranen.bananen@gmail.com>
2018-01-09 11:33:14 +0100

    Fixed typo (maybe)

--- static/remoteMap.js
+++ static/remoteMap.js
@@ -1,10 +1,10 @@
 function getParameterByName(name, url) {
-    if (!url) url = window.location.href;
+    if(!url) url = window.location.href;
     name = name.replace(/[\[\]]/g, "\\$&");
     var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
         results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
+    if(!results) return null;
+    if(!results[2]) return '';
     return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 remoteMapConfig = {
@@ -226,8 +226,9 @@ for(let i = 1; i <= 3; i++){
 			x:-1,
 			y:-1,
 		},
-	},
-	entityDrawRules["assembling-machine-"+i] = assemblingMachineTemplate;
+	};
+	let name = "assembling-machine-"+i;
+	entityDrawRules[name] = assemblingMachineTemplate;
 }
 function drawEntity(entity, dontCache){
 	if(entity.x && entity.y){
