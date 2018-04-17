0edb4caff9002ffa3ea1be2fc888ee652ef3c977
psihius <arvids.godjuks@gmail.com>
2018-04-17 14:26:09 +0300

    Typo fix

--- sharedPlugins/researchSync/enableResearch.lua
+++ sharedPlugins/researchSync/enableResearch.lua
@@ -1,6 +1,6 @@
 /silent-command
 if game.forces['player'].technologies['{tech_name}'] then
-	game.forces['player'].technologies['{tech_name}}'].researched={tech_researched}
-	game.forces['player'].technologies['{tech_name}}'].level={tech_level}
+	game.forces['player'].technologies['{tech_name}'].researched={tech_researched}
+	game.forces['player'].technologies['{tech_name}'].level={tech_level}
 	game.print("researcSync enabled research: {tech_name} to level {tech_level}")
 end
