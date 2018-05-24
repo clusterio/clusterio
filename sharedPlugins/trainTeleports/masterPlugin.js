1da188cb0de2f997721bb23d408ba5a634e19af2
Danielv123 <tranen.bananen@gmail.com>
2018-05-24 22:06:00 +0200

    More work on trainTeleports master plugin

--- sharedPlugins/trainTeleports/masterPlugin.js
+++ sharedPlugins/trainTeleports/masterPlugin.js
@@ -1,3 +1,46 @@
+/*jslint esnext:true*/
+
+class trainTeleporter{
+	constructor({socket, instanceID, master}){
+		this.socket = socket;
+		this.instanceID = instanceID;
+		this.master = master;
+		
+		(async () => {
+			this.socket.on("trainstop_added", data => {
+				await this.addTrainstop(data);
+				console.log("trainstop_added: "+data.name);
+			});
+			this.socket.on("trainstop_edited", data => {
+				await this.removeTrainstop(data);
+				await this.addTrainstop(data);
+				console.log("trainstop_edited: "+data.name);
+			});
+			this.socket.on("trainstop_removed", data => {
+				await this.removeTrainstop(data);
+				console.log("trainstop_removed: "+data.name);
+			});
+		})();
+	}
+	async addTrainstop({x, y, name}){
+		let trainstops = await this.master.getTrainstops();
+		if(!trainstops[name]) trainstops[name] = {name, stops:[]};
+		trainstops[data.name].stops.push({x,y});
+		return true;
+	}
+	async removeTrainstop({x, y, name}){
+		let trainstops = await this.master.getTrainstops();
+		if(!trainstops[name]) return true;
+		
+		trainstops.data.forEach(trainstop, index => {
+			if(trainstop.x == x && trainstop.y == y){
+				delete trainstops[index];
+			}
+		});
+		resolve(true);
+	}
+}
+
 class masterPlugin {
 	constructor({config, pluginConfig, path, socketio, express}){
 		this.config = config;
@@ -6,9 +49,14 @@ class masterPlugin {
 		this.io = socketio;
 		this.express = express;
 		
+		this.clients = {};
 		this.io.on("connection", socket => {
-			socket.on("registerTrain", ()=>{
-				
+			socket.on("registerTrainTeleporter", data => {
+				this.clients[data.instanceID] = new trainTeleporter({
+					master:this,
+					instanceID: data.instanceID,
+					socket,
+				});
 			});
 		});
 	}
