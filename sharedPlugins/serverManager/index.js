e2566d2d1c9c45c86c352de8f570e4fdae243bf9
Daniel Vestøl <tranen.bananen@gmail.com>
2018-09-27 14:35:41 +0200

    [big change] Added serverManager plugin

---
+++ sharedPlugins/serverManager/index.js
@@ -0,0 +1,43 @@
+const needle = require("needle");
+const fs = require("fs-extra");
+
+const pluginConfig = require("./config");
+const COMPRESS_LUA = false;
+
+module.exports = class remoteCommands {
+	constructor(mergedConfig, messageInterface, extras){
+		this.messageInterface = messageInterface;
+		this.config = mergedConfig;
+		this.socket = extras.socket;
+		const pluginManager = require("./../../lib/manager/pluginManager.js")(this.config);
+		const configManager = require("./../../lib/manager/configManager.js");
+		// const modManager = require("./../../lib/manager/modManager.js")(this.config);
+		this.socket.on("hello", () => {
+			this.socket.emit("registerServerManager");
+		});
+		this.socket.on("serverManagerGetStatus", async data => {
+			
+		});
+		this.socket.on("serverManagerGetMods", async (data, callback) => {
+			// TODO: Implement this function separately from listMods (which console.logs it)
+			// let mods = modManager.getMods()
+			// callback(mods);
+		});
+		this.socket.on("serverManagerGetConfig", async (data, callback) => {
+			let config = await configManager.getConfig(data.instance);
+			callback(config);
+		});
+		this.socket.on("serverManagerEditConfig", async (data, callback) => {
+			callback(configManager.editConfig(data.instance, data.entry, data.value));
+		});
+		this.socket.on("serverManagerGetPlugins", async (data, callback) => {
+			callback(await pluginManager.getPlugins());
+		});
+		this.socket.on("serverManagerEnablePlugin", async (data, callback) => {
+			callback(await pluginManager.enablePlugin(data.name, data.instanceID /* Currently ignored */));
+		});
+		this.socket.on("serverManagerDisablePlugin", async (data, callback) => {
+			callback(await pluginManager.disablePlugin(data.name, data.instanceID /* Currently ignored */));
+		});
+	}
+}
---
+++ sharedPlugins/serverManager/js/api-endpoints.js
@@ -0,0 +1,127 @@
+module.exports = that => {
+	/**
+	 - Enable a plugin by deleting the DISABLED file
+	 - Use socket.io to update masters list of plugins for this server
+	
+	Give standard JSON object response
+	*/
+	that.app.post("/api/serverManager/enablePlugin", async (req, res) => {
+		if((req.body.token || req.cookies.token)
+		&& req.body.instanceID
+		&& typeof req.body.instanceID == "string"
+		&& req.body.pluginName
+		&& typeof req.body.pluginName == "string"
+		&& that.playerManager){
+			let perms = await that.playerManager.main.getPermissions((req.body.token || req.cookies.token));
+			if(perms.cluster.includes("enablePlugin")
+			|| (perms.instance[req.body.instanceID]
+			&& perms.instance[req.body.instanceID].includes("enablePlugin"))){
+				that.serverSockets[req.body.instanceID].socket.emit("serverManagerEnablePlugin", {
+					name: req.body.pluginName,
+					instanceID: req.body.instanceID,
+				}, async resp => {
+					if(resp.ok){
+						await updateData("plugins", req.body.instanceID);
+					}
+					res.send(resp);
+				});
+			} else {
+				res.send({
+					ok:false,
+					msg:"Insufficient permissions",
+				});
+			}
+		} else {
+			res.send({
+				ok:false,
+				msg:"Invalid request body",
+			});
+		}
+	});
+	/**
+	 - Disable a plugin by adding an empty file named DISABLED to the plugins install pathname
+	 - Use socket.io to update masters lost of plugins for this server
+	 
+	Give standard JSON object response
+	*/
+	that.app.post("/api/serverManager/disablePlugin", async (req, res) => {
+		if((req.body.token || req.cookies.token)
+		&& req.body.instanceID
+		&& typeof req.body.instanceID == "string"
+		&& req.body.pluginName
+		&& typeof req.body.pluginName == "string"
+		&& that.playerManager){
+			let perms = await that.playerManager.main.getPermissions((req.body.token || req.cookies.token));
+			if(perms.cluster.includes("disablePlugin")
+			|| (perms.instance[req.body.instanceID]
+			&& perms.instance[req.body.instanceID].includes("disablePlugin"))){
+				that.serverSockets[req.body.instanceID].socket.emit("serverManagerDisablePlugin", {
+					name: req.body.pluginName,
+					instanceID: req.body.instanceID,
+				}, async resp => {
+					if(resp.ok){
+						await updateData("plugins", req.body.instanceID);
+					}
+					res.send(resp);
+				});
+			} else {
+				res.send({
+					ok:false,
+					msg:"Insufficient permissions",
+				});
+			}
+		} else {
+			res.send({
+				ok:false,
+				msg:"Invalid request body",
+			});
+		}
+	});
+	that.app.post("/api/serverManager/editConfig", async (req,res) => {
+		if((req.body.token || req.cookies.token)
+		&& req.body.instanceID
+		&& typeof req.body.instanceID == "string"
+		&& req.body.entry
+		&& typeof req.body.entry == "string"
+		&& req.body.value !== undefined
+		&& that.playerManager){
+			let perms = await that.playerManager.main.getPermissions((req.body.token || req.cookies.token));
+			if(perms.cluster.includes("writeConfig")
+			|| (perms.instance[req.body.instanceID]
+			&& perms.instance[req.body.instanceID].includes("writeConfig"))){
+				that.serverSockets[req.body.instanceID].socket.emit("serverManagerEditConfig", {
+					entry: req.body.entry,
+					value: req.body.value,
+				}, async resp => {
+					if(resp.ok){
+						// Retrieve the latest config from the server and store it
+						await updateData("config", req.body.instanceID);
+					}
+					res.send(resp);
+				});
+			} else {
+				res.send({
+					ok:false,
+					msg:"Insufficient permissions",
+				});
+			}
+		} else {
+			res.send({
+				ok:false,
+				msg:"Invalid request body",
+			});
+		}
+	});
+	async function updateData(type, instanceID){
+		// Retrieve the latest ${type} from the server and store it
+		if(type == "config"){
+			that.serverSockets[instanceID].socket.emit("serverManagerGetConfig", {}, data => {
+				that.serverSockets[instanceID].config = data;
+			});
+		} else if(type == "plugins"){
+			that.serverSockets[instanceID].socket.emit("serverManagerGetPlugins", {}, data => {
+				that.serverSockets[instanceID].plugins = data;
+			});
+		}
+	}
+}
