ac1eee7a551c18a090d9e555c4b6a34dee491ee7
psihius <arvids.godjuks@gmail.com>
2019-07-14 17:21:14 +0200

    Dump only enabled research

--- sharedPlugins/researchSync/dumpResearch.lua
+++ sharedPlugins/researchSync/dumpResearch.lua
@@ -3,7 +3,7 @@
 local data = {};
 
 for _, tech in pairs(game.forces["player"].technologies) do
-    if tech.prototype.max_level == 4294967295 then
+    if tech.enabled == true and tech.prototype.max_level == 4294967295 then
         table.insert(data, tech.name .. ":" .. tostring(tech.researched) .. ":" .. tostring(tech.level) .. ":true");
     else
         table.insert(data, tech.name .. ":" .. tostring(tech.researched) .. ":" .. tostring(tech.level) .. ":false");
