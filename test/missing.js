1fd8ae954b0cd9c155638acb67f57e63a3980367
Hornwitser <github@hornwitser.no>
2019-09-15 17:09:53 +0200

    Add missing files to coverage report
    Add missing.js test files that requires any files that currently does
    not have any unit tests defined for them.  This should hopefully make it
    so that when unit tests are added for parts of these files the coverage
    will increase instead of decreasing.
    
    Note that this does not include CLI Tools/ as those are not testable.

---
+++ test/missing.js
@@ -0,0 +1,12 @@
+// These are libraries that does not have any units test, they are
+// required here in order to track coverage for them.
+
+require('lib/clusterTools');
+require('lib/dole_nn_base');
+require('lib/getInstances');
+require('lib/hash');
+require('lib/manager/instanceManager');
+require('lib/manager/modManager');
+require('lib/manager/pluginManager');
+require('lib/npmPostinstall.js');
+require('lib/typedef');
