278dc9b2d2818a3c354279ca1cebe935e7bef353
Hornwitser <github@hornwitser.no>
2019-11-26 18:44:10 +0100

    Remove empty instanceManager file
    Remove the empty/incomplete instanceManager implementation.

--- test/missing.js
+++ test/missing.js
@@ -1,7 +1,6 @@
 // These are libraries that does not have any units test, they are
 // required here in order to track coverage for them.
 
-require('lib/manager/instanceManager');
 require('lib/manager/modManager');
 require('lib/manager/pluginManager');
 require('lib/npmPostinstall.js');
