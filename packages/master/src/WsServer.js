clusterio/clusterio

commit 67219ede6b6d9395abbaaf365db241c205c68b63

Refactor master code
Move implementation of master systems into dedicated classes for better
organization.

packages/master/src/WsServer.js
@@ -1,0 +1,302 @@
+"use strict";
+const jwt = require("jsonwebtoken");
+const WebSocket = require("ws");
+
+const { logger } = require("@clusterio/lib/logging");
+const libPrometheus = require("@clusterio/lib/prometheus");
+const libSchema = require("@clusterio/lib/schema");
+
+const ControlConnection = require("./ControlConnection");
+const packageVersion = require("../package").version;
+const SlaveConnection = require("./SlaveConnection");
+const WsServerConnector = require("./WsServerConnector");
+
+
+const wsMessageCounter = new libPrometheus.Counter(
+	"clusterio_master_websocket_message_total",
+	"How many messages have been received over WebSocket on the master server",
+	{ labels: ["direction"] }
+);
+
+const wsConnectionsCounter = new libPrometheus.Counter(
+	"clusterio_master_websocket_connections_total",
+	"How many WebSocket connections have been initiated on the master server"
+);
+
+const wsRejectedConnectionsCounter = new libPrometheus.Counter(
+	"clusterio_master_websocket_rejected_connections_total",
+	"How many WebSocket connections have been rejected during the handshake on the master server"
+);
+
+/**
+ * WebSocket server
+ * @alias module:master/src/WsServer
+ */
+class WsServer {
+	constructor(master) {
+		this.master = master;
+
+		this.stopAcceptingNewSessions = false;
+
+		/** @type {Array<module:master/src/ControlConnection>} */
+		this.controlConnections = [];
+		/** @type {Map<number, module:master/src/SlaveConnection>} */
+		this.slaveConnections = new Map();
+		/** @type {Map<number, module:master/src/WsServerConnector>} */
+		this.activeConnectors = new Map();
+		/** @type {Set<module:ws>} */
+		this.pendingSockets = new Set();
+
+		// Unique string for the session token audience
+		this.sessionAud = `session-${Date.now()}`;
+		this.nextSessionId = 1;
+
+		/** @type {module:ws.Server} */
+		this.wss = new WebSocket.Server({
+			noServer: true,
+			path: "/api/socket",
+		});
+		this.wss.on("connection", (socket, req) => this.handleConnection(socket, req));
+	}
+
+	async stop() {
+		this.stopAcceptingNewSessions = true;
+
+		let disconnectTasks = [];
+		for (let controlConnection of this.controlConnections) {
+			controlConnection.connector.setTimeout(this.master.config.get("master.connector_shutdown_timeout"));
+			disconnectTasks.push(controlConnection.disconnect(1001, "Server Quit"));
+		}
+
+		for (let slaveConnection of this.slaveConnections.values()) {
+			slaveConnection.connector.setTimeout(this.master.config.get("master.connector_shutdown_timeout"));
+			disconnectTasks.push(slaveConnection.disconnect(1001, "Server Quit"));
+		}
+
+		logger.info(`WsServer | Waiting for ${disconnectTasks.length} connectors to close`);
+		for (let task of disconnectTasks) {
+			try {
+				await task;
+			} catch (err) {
+				if (!(err instanceof libErrors.SessionLost)) {
+					logger.warn(`Unexpected error disconnecting connector:\n${err.stack}`);
+				}
+			}
+		}
+
+		for (let socket of this.pendingSockets) {
+			socket.close(1001, "Server Quit");
+		}
+	}
+
+	handleUpgrade(req, socket, head) {
+		// For reasons that defy common sense, the connection event has
+		// to be emitted explictly when using noServer.
+		this.wss.handleUpgrade(req, socket, head, (ws) => {
+			this.wss.emit("connection", ws, req);
+		});
+	}
+
+	handleConnection(socket, req) {
+		logger.verbose(`WsServer | new connection from ${req.socket.remoteAddress}`);
+
+		// Track statistics
+		wsConnectionsCounter.inc();
+		socket.on("message", (message) => {
+			wsMessageCounter.labels("in").inc();
+			if (!socket.clusterio_ignore_dump) {
+				this.master.debugEvents.emit("message", { direction: "in", content: message });
+			}
+		});
+		let originalSend = socket.send;
+		socket.send = (...args) => {
+			wsMessageCounter.labels("out").inc();
+			if (typeof args[0] === "string" && !socket.clusterio_ignore_dump) {
+				this.master.debugEvents.emit("message", { direction: "out", content: args[0] });
+			}
+			return originalSend.call(socket, ...args);
+		};
+
+		// Start connection handshake.
+		let loadedPlugins = {};
+		for (let [name, plugin] of this.master.plugins) {
+			loadedPlugins[name] = plugin.info.version;
+		}
+
+		socket.send(JSON.stringify({ seq: null, type: "hello", data: {
+			version: packageVersion,
+			plugins: loadedPlugins,
+		}}));
+
+		this.attachHandler(socket, req);
+	}
+
+	attachHandler(socket, req) {
+		this.pendingSockets.add(socket);
+
+		let timeoutId = setTimeout(() => {
+			logger.verbose(`WsServer | closing ${req.socket.remoteAddress} after timing out on handshake`);
+			wsRejectedConnectionsCounter.inc();
+			socket.close(1008, "Handshake timeout");
+			this.pendingSockets.delete(socket);
+		}, 30*1000);
+
+		socket.once("message", (message) => {
+			clearTimeout(timeoutId);
+			this.pendingSockets.delete(socket);
+			this.handleHandshake(
+				message, socket, req
+			).catch(err => {
+				logger.error(`
++----------------------------------------------------------------+
+| Unexpected error occured in WebSocket handshake, please report |
+| it to https://github.com/clusterio/factorioClusterio/issues    |
++----------------------------------------------------------------+
+${err.stack}`
+				);
+				wsRejectedConnectionsCounter.inc();
+				socket.close(1011, "Unexpected error");
+			});
+		});
+	}
+
+	async handleHandshake(message, socket, req) {
+		try {
+			message = JSON.parse(message);
+		} catch (err) {
+			logger.verbose(`WsServer | closing ${req.socket.remoteAddress} after receiving invalid JSON`);
+			wsRejectedConnectionsCounter.inc();
+			socket.close(1002, "Invalid JSON");
+			return;
+		}
+
+		if (!libSchema.clientHandshake(message)) {
+			logger.verbose(`WsServer | closing ${req.socket.remoteAddress} after receiving invalid handshake`);
+			wsRejectedConnectionsCounter.inc();
+			socket.close(1002, "Bad handshake");
+			return;
+		}
+
+		let { seq, type, data } = message;
+
+		if (type === "resume") {
+			let connector;
+			try {
+				let payload = jwt.verify(
+					data.session_token,
+					this.master.config.get("master.auth_secret"),
+					{ audience: this.sessionAud }
+				);
+
+				connector = this.activeConnectors.get(payload.sid);
+				if (!connector) {
+					throw new Error();
+				}
+
+			} catch (err) {
+				socket.send(JSON.stringify({ seq: null, type: "invalidate", data: {}}));
+				this.attachHandler(socket, req);
+				return;
+			}
+
+			connector.continue(socket, data.last_seq);
+			return;
+		}
+
+		if (this.stopAcceptingNewSessions) {
+			logger.verbose(`WsServer | closing ${req.socket.remoteAddress}, server is shutting down`);
+			wsRejectedConnectionsCounter.inc();
+			socket.close(1001, "Shutting down");
+			return;
+		}
+
+		// Validate token
+		let user;
+		try {
+			if (type === "register_slave") {
+				let tokenPayload = jwt.verify(
+					data.token,
+					this.master.config.get("master.auth_secret"),
+					{ audience: "slave" }
+				);
+
+				if (tokenPayload.slave !== data.id) {
+					throw new Error("missmatched slave id");
+				}
+
+			} else if (type === "register_control") {
+				let tokenPayload = jwt.verify(
+					data.token,
+					this.master.config.get("master.auth_secret"),
+					{ audience: "user" }
+				);
+
+				user = this.master.userManager.users.get(tokenPayload.user);
+				if (!user) {
+					throw new Error("invalid user");
+				}
+				if (tokenPayload.iat < user.tokenValidAfter) {
+					throw new Error("invalid token");
+				}
+				user.checkPermission("core.control.connect");
+			}
+
+		} catch (err) {
+			logger.verbose(`WsServer | authentication failed for ${req.socket.remoteAddress}`);
+			wsRejectedConnectionsCounter.inc();
+			socket.close(4003, `Authentication failed: ${err.message}`);
+			return;
+		}
+
+		let sessionId = this.nextSessionId;
+		this.nextSessionId += 1;
+		let sessionToken = jwt.sign(
+			{ aud: this.sessionAud, sid: sessionId }, this.master.config.get("master.auth_secret")
+		);
+		let heartbeatInterval = this.master.config.get("master.heartbeat_interval");
+		let connector = new WsServerConnector(socket, sessionId, heartbeatInterval);
+		this.activeConnectors.set(sessionId, connector);
+		connector.on("close", () => {
+			this.activeConnectors.delete(connector);
+		});
+
+		let additionalReadyData = {};
+		if (type === "register_slave") {
+			let connection = this.slaveConnections.get(data.id);
+			if (connection) {
+				logger.verbose(`WsServer | disconnecting existing connection for slave ${data.id}`);
+				connection.connector.setTimeout(15); // Slave connection is likely stalled
+				await connection.disconnect(1008, "Registered from another connection");
+			}
+
+			logger.verbose(`WsServer | registered slave ${data.id} version ${data.version}`);
+			connection = new SlaveConnection(data, connector, this.master);
+			connector.on("close", () => {
+				if (this.slaveConnections.get(data.id) === connection) {
+					this.slaveConnections.delete(data.id);
+				}
+			});
+			this.slaveConnections.set(data.id, connection);
+
+
+		} else if (type === "register_control") {
+			logger.verbose(`WsServer | registered control from ${req.socket.remoteAddress}`);
+			let connection = new ControlConnection(data, connector, this.master, user);
+			connector.on("close", () => {
+				let index = this.controlConnections.indexOf(this);
+				if (index !== -1) {
+					this.controlConnections.splice(index, 1);
+				}
+			});
+			this.controlConnections.push(connection);
+			additionalReadyData["account"] = {
+				name: user.name,
+			};
+		}
+
+		connector.ready(sessionToken, additionalReadyData);
+	}
+
+}
+
+module.exports = WsServer;

packages/lib/plugin.js
@@ -191,7 +191,7 @@ class BaseInstancePlugin {
 	 * Plugins must stop sending messages to the master, or are forwarded
 	 * via the master server after the prepare disconnect has been handled.
 	 *
-	 * @param {module:master/master~SlaveConnection} connection -
+	 * @param {module:master/src/SlaveConnection} connection -
 	 *     The connection to the slave preparing to disconnect.
 	 */
 	async onPrepareMasterDisconnect(connection) { }
@@ -226,6 +226,11 @@ class BaseInstancePlugin {
 class BaseMasterPlugin {
 	constructor(info, master, metrics, logger) {
 		this.info = info;
+
+		/**
+		 * Master server
+		 * @type {module:master/src/Master}
+		 */
 		this.master = master;
 		this.metrics = metrics;
 
@@ -323,7 +328,7 @@ class BaseMasterPlugin {
 	 *
 	 * Invoked when the connection to the slave has been closed.
 	 *
-	 * @param {module:master/master~SlaveConnection} connection -
+	 * @param {module:master/src/SlaveConnection} connection -
 	 *     The connection the event occured on.
 	 * @param {string} event - one of connect, drop, resume and close
 	 */
@@ -359,7 +364,7 @@ class BaseMasterPlugin {
 	 *
 	 * Invoked when the connection to the control has been closed.
 	 *
-	 * @param {module:master/master~ControlConnection} connection -
+	 * @param {module:master/src/ControlConnection} connection -
 	 *     The connection the event occured on.
 	 * @param {string} event - one of connect, drop, resume, and close.
 	 */
@@ -375,7 +380,7 @@ class BaseMasterPlugin {
 	 * Plugins must stop sending messages to the slave in question after the
 	 * prepare disconnect has been handled.
 	 *
-	 * @param {module:master/master~SlaveConnection} connection -
+	 * @param {module:master/src/SlaveConnection} connection -
 	 *     The connection to the slave preparing to disconnect.
 	 */
 	async onPrepareSlaveDisconnect(connection) { }
@@ -404,7 +409,7 @@ class BaseMasterPlugin {
 	 * @param {Object} data - Data ta pass with the event.
 	 */
 	broadcastEventToSlaves(event, data={}) {
-		for (let slaveConnection of this.master.slaveConnections.values()) {
+		for (let slaveConnection of this.master.wsServer.slaveConnections.values()) {
 			if (
 				!slaveConnection.connector.closing
 				&& (!event.plugin || slaveConnection.plugins.has(event.plugin))

packages/master/src/BaseConnection.js
@@ -1,0 +1,95 @@
+"use strict";
+const libErrors = require("@clusterio/lib/errors");
+const libLink = require("@clusterio/lib/link");
+const { logger } = require("@clusterio/lib/logging");
+const libPlugin = require("@clusterio/lib/plugin");
+
+
+/**
+ * Base class for master server connections
+ *
+ * @extends module:lib/link.Link
+ * @alias module:master/src/BaseConnection
+ */
+class BaseConnection extends libLink.Link {
+	constructor(target, connector, master) {
+		/** @member {module:master/src/WsServerConnector} module:master/src/BaseConnection#connector */
+		super("master", target, connector);
+		this._master = master;
+		libLink.attachAllMessages(this);
+		for (let masterPlugin of this._master.plugins.values()) {
+			libPlugin.attachPluginMessages(this, masterPlugin);
+		}
+	}
+
+	async forwardRequestToInstance(message, request) {
+		let instance = this._master.instances.get(message.data.instance_id);
+		if (!instance) {
+			throw new libErrors.RequestError(`Instance with ID ${message.data.instance_id} does not exist`);
+		}
+
+		let slaveId = instance.config.get("instance.assigned_slave");
+		if (slaveId === null) {
+			throw new libErrors.RequestError("Instance is not assigned to a slave");
+		}
+
+		let connection = this._master.wsServer.slaveConnections.get(slaveId);
+		if (!connection) {
+			throw new libErrors.RequestError("Slave containing instance is not connected");
+		}
+		if (request.plugin && !connection.plugins.has(request.plugin)) {
+			throw new libErrors.RequestError(`Slave containing instance does not have ${request.plugin} plugin`);
+		}
+
+		return await request.send(connection, message.data);
+	}
+
+	async forwardEventToInstance(message, event) {
+		let instance = this._master.instances.get(message.data.instance_id);
+		if (!instance) { return; }
+
+		let slaveId = instance.config.get("instance.assigned_slave");
+		if (slaveId === null) { return; }
+
+		let connection = this._master.wsServer.slaveConnections.get(slaveId);
+		if (!connection || connection.closing) { return; }
+		if (event.plugin && !connection.plugins.has(event.plugin)) { return; }
+
+		event.send(connection, message.data);
+	}
+
+	async broadcastEventToSlaves(message, event) {
+		for (let slaveConnection of this._master.wsServer.slaveConnections.values()) {
+			// Do not broadcast back to the source
+			if (slaveConnection === this) { continue; }
+			if (slaveConnection.connector.closing) { continue; }
+			if (event.plugin && !slaveConnection.plugins.has(event.plugin)) { continue; }
+
+			event.send(slaveConnection, message.data);
+		}
+	}
+
+	async broadcastEventToInstance(message, event) {
+		await this.broadcastEventToSlaves(message, event);
+	}
+
+	async prepareDisconnectRequestHandler(message, request) {
+		await libPlugin.invokeHook(this._master.plugins, "onPrepareSlaveDisconnect", this);
+		this.connector.setClosing();
+		return await super.prepareDisconnectRequestHandler(message, request);
+	}
+
+	async disconnect(code, reason) {
+		try {
+			await libLink.messages.prepareDisconnect.send(this);
+		} catch (err) {
+			if (!(err instanceof libErrors.SessionLost)) {
+				logger.error(`"Unexpected error preparing disconnect:\n${err.stack}`);
+			}
+		}
+
+		await this.connector.close(code, reason);
+	}
+}
+
+module.exports = BaseConnection;

commit b019d44d0bd077c184108b11a5273361fefd4596

Fix active connectors leaking after refactor
The activeConnectors mapping is from session id to connector, but the
master refactor in 67219ed accidentally changed it to try deleting the
entry by the connector reference instead.  Possibly mistaking it for a
Set instead of a Map.

packages/master/src/WsServer.js
@@ -258,7 +258,7 @@ ${err.stack}`
 		let connector = new WsServerConnector(sessionId, sessionTimeout, heartbeatInterval);
 		this.activeConnectors.set(sessionId, connector);
 		connector.on("close", () => {
-			this.activeConnectors.delete(connector);
+			this.activeConnectors.delete(sessionId);
 		});
 
 		let additionalReadyData = {};