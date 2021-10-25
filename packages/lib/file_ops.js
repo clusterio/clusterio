69107373741447db774cd89877e625042b407177
jake87 <jakehfreebsd@gmail.com>
2021-10-25 21:58:56 +1100

    Added function in lib/file_ops.js to return a temp filename with salt

--- packages/lib/file_ops.js
+++ packages/lib/file_ops.js
@@ -4,7 +4,7 @@
 "use strict";
 const fs = require("fs-extra");
 const path = require("path");
-
+const crypto = require("crypto"); // needed for getTempFile
 /**
  * Returns the newest file in a directory
  *
@@ -68,8 +68,29 @@ async function findUnusedName(directory, name, extension = "") {
 		}
 	}
 }
+/**
+ * Returns a path to a temporary file.
+ *
+ * Warning: this function should not be relied upon to be accurate for
+ * security sensitive applications.  The selection process is inherently
+ * racy and a file or folder may have been created in the folder by the time
+ * this function returns.
+ *
+ * @param {string} prefix - prefix for file
+ * @param {string} suffix - suffix for file
+ * @param {string} tmpdir - directory for file
+ */
+async function getTempFile(prefix, suffix, tmpdir) {
+	prefix = (typeof prefix !== "undefined") ? prefix : "tmp.";
+	suffix = (typeof suffix !== "undefined") ? suffix : "";
+	tmpdir = tmpdir ? tmpdir : os.tmpdir();
+	return path.join(tmpdir, prefix + crypto.randomBytes(16).toString("hex") + suffix);
+	// IMO, 16 bytes of random hex should be fine, and hex is allowable in any filesystem, but if
+	// some other system is better, please inform
+}
 
 module.exports = {
 	getNewestFile,
 	findUnusedName,
+	getTempFile,
 };
