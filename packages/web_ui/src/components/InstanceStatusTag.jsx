b86305db8c3cc619e26bbada372104d9a1ed9ee9
bbassie <badgamernl@gmail.com>
2021-11-16 23:42:02 +0100

    Updated colors of the instance tag

--- packages/web_ui/src/components/InstanceStatusTag.jsx
+++ packages/web_ui/src/components/InstanceStatusTag.jsx
@@ -2,20 +2,20 @@ import { Tag } from "antd";
 import React from "react";
 
 const statusColors = {
-	unknown: "#d46b08",
-	unassigned: "#d4380d",
+	unknown: "#8c8c8c",
+	unassigned: "#eb2f96",
 	stopped: "#cf1322",
-	starting: "#d4b106",
+	starting: "#ad8b00",
 	running: "#52c41a",
 	stopping: "#d48806",
 	creating_save: "#096dd9",
 	exporting_data: "#08979c",
-	deleted: "#cf1322",
+	deleted: "#262626",
 };
 
 
 export default function InstanceStatusTag(props) {
 	return <Tag color={statusColors[props.status]}>
-		{props.status}
+		{props.status.replace("_", " ")}
 	</Tag>;
 }
