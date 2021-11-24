dfd0cb2d36d41939a841ada1821695c51a2b8785
J87 <68929154+jake-87@users.noreply.github.com>
2021-11-25 07:45:56 +1100

    Fix linter issue

--- packages/ctl/ctl.js
+++ packages/ctl/ctl.js
@@ -437,8 +437,8 @@ instanceConfigCommands.add(new libCommand.Command({
 		}
 		let disallowedList = {"instance.id": 0, "instance.assigned_slave": 0, "factorio.settings": 0};
 		let allConfigElements = await serializedConfigToString(
-			respose.serialized_config, 
-			libConfig.InstanceConfig, 
+			respose.serialized_config,
+			libConfig.InstanceConfig,
 			disallowedList
 		);
 		await fs.writeFile(tmpFile, allConfigElements, (err) => {
