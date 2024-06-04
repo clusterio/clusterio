e7ef6540750f86d85c1f92f6c8b2eb64c71260fe
Cooldude2606 <25043174+Cooldude2606@users.noreply.github.com>
2024-06-04 23:36:40 +0100

    Add comments to empty instance template

--- packages/create/templates/plugin-js/instance_empty.js
+++ packages/create/templates/plugin-js/instance_empty.js
@@ -2,6 +2,8 @@
 const { BaseInstancePlugin } = require("@clusterio/host");
 
 class InstancePlugin extends BaseInstancePlugin {
+	// This class is empty because an instance plugin must be defined for a module to be injected
+	// This requirement may change in the future to allow for standalone modules
 }
 
 module.exports = {
