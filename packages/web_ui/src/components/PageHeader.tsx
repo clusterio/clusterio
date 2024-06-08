1794a6679bfe4069f154be607770e7c320a1eb00
Danielv123 <danielv@danielv.no>
2024-06-08 06:39:58 +0100

    Extend PageHeader for plugins
    My plugins used the antd pageheader more than base did. I did not notice that these features dissappeared when we migrated from antd to our own implementation - this brings them back

--- packages/web_ui/src/components/PageHeader.tsx
+++ packages/web_ui/src/components/PageHeader.tsx
@@ -1,13 +1,19 @@
 import React from "react";
+import { Space, Typography } from "antd";
+const { Text } = Typography;
 
 type PageHeaderProps = {
-	title: string|React.ReactElement;
+	title: string | React.ReactElement;
+	subTitle?: string | React.ReactElement;
 	extra?: React.JSX.Element;
 };
 
 export default function PageHeader(props: PageHeaderProps): React.JSX.Element {
 	return <div className="page-header">
-		<h2 className="page-header-title">{props.title}</h2>
+		<Space>
+			<h2 className="page-header-title">{props.title}</h2>
+			{props.subTitle && <Text type="secondary">{props.subTitle}</Text>}
+		</Space>
 		{props.extra && <div className="page-header-extra">{props.extra}</div>}
 	</div>;
 }
