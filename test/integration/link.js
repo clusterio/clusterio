ebc33f84cb245ce8f92a05a4d077b499420b30aa
Cooldude2606 <25043174+Cooldude2606@users.noreply.github.com>
2024-09-13 23:14:55 +0100

    Additional Tests

--- test/integration/link.js
+++ test/integration/link.js
@@ -4,15 +4,17 @@ const events = require("events");
 const fs = require("fs-extra");
 const jwt = require("jsonwebtoken");
 
-const { TestControlConnector, TestControl, get, exec, url } = require("./index");
-const { ConnectionClosed, ProtocolViolation, PolicyViolation, AuthenticationFailed } = require("@clusterio/lib");
+const { TestControlConnector, TestHostConnector, TestControl, get, exec, url, slowTest } = require("./index");
+const { ConnectionClosed, ProtocolError, PolicyViolation, AuthenticationFailed } = require("@clusterio/lib");
 
 let token = jwt.sign({ aud: "user", user: "test" }, Buffer.from("TestSecretDoNotUse", "base64"));
+let tokenHost = jwt.sign({ aud: "host", host: 0 }, Buffer.from("TestSecretDoNotUse", "base64"));
 
 describe("Integration of lib/link", function() {
 	let tlsCa;
 	let control;
 	let controlConnector;
+	let hostConnector;
 	before(async function() {
 		tlsCa = await fs.readFile("test/file/tls/cert.pem");
 	});
@@ -20,9 +22,13 @@ describe("Integration of lib/link", function() {
 		controlConnector = new TestControlConnector(url, 0.2, tlsCa);
 		controlConnector.token = token;
 		control = new TestControl(controlConnector, false);
+		hostConnector = new TestHostConnector(url, 0.2, tlsCa);
+		hostConnector.token = tokenHost;
+		hostConnector.hostId = 0;
 	});
 	afterEach(async function() {
 		await controlConnector.disconnect();
+		await hostConnector.disconnect();
 	});
 
 	it("should emitt an error if authentication failed", async function() {
@@ -97,5 +103,46 @@ describe("Integration of lib/link", function() {
 			controlConnector.close();
 		});
 		await events.once(controlConnector, "close");
+	});
+
+	it("should refuse connection with incorrect audience", async function() {
+		hostConnector.token = token;
+		await assert.rejects(
+			hostConnector.connect(),
+			new Error("Authentication failed: jwt audience invalid. expected: host or slave")
+		);
+	});
+
+	it("should refuse connection with mismatched ids", async function() {
+		hostConnector.hostId = 1;
+		await assert.rejects(
+			hostConnector.connect(),
+			new Error("Authentication failed: missmatched host id")
+		);
+	});
+
+	it("should close old connections from the same address", async function() {
+		slowTest(this);
+		const hostConnectorFirst = new TestHostConnector(url, 0.2, tlsCa);
+		hostConnectorFirst.token = hostConnector.token;
+		hostConnectorFirst.hostId = hostConnector.hostId;
+		const onceClose = events.once(hostConnectorFirst, "close");
+		await hostConnectorFirst.connect();
+		await hostConnector.connect();
+		await assert.rejects(
+			onceClose,
+			new PolicyViolation("Newer connection for host from same address")
+		);
+	});
+
+	it("should refuse duplicate host connections", async function() {
+		this.skip(); // Can't be checked because the remote address for both connections is the same
+
+		hostConnector.token = jwt.sign({ aud: "host", host: 4 }, Buffer.from("TestSecretDoNotUse", "base64"));
+		hostConnector.hostId = 4; // Same as already running host
+		await assert.rejects(
+			hostConnector.connect(),
+			new Error("Authentication failed: missmatched host id")
+		);
 	});
 });
